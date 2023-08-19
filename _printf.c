474c6013ce51e37d7f554e9f5aca267be7d0269f
rahim daud <rahimdaud246@gmail.com>
2023-08-19 15:52:00 +0000

    second commit

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * _printf - ...
+ * @format: ...
+ * @...: ...
+ */
+void _printf(const char *format, ...)
+{
+
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef PRINT_F_H
+#define PRINT_F_H
+
+int _putchar(char c);
+void _printf(char *format, ...);
+
+#endif
